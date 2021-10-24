cd4d65bae9df9bdc907ca8ebd48afe99699ddb63
JhonLloyd <jhonlloydgellica15@gmail.com>
2021-10-24 12:25:34 +0800

    About section done

--- JS/app.js
+++ JS/app.js
@@ -2,10 +2,4 @@ $(document).ready(() => {
   $(".fa-bars").click(() => {
     $(".main-navigation").slideToggle();
   });
-
-  VanillaTilt.init(document.querySelectorAll(".box-1"), {
-    max: 30,
-    speed: 400,
-    transition: true,
-  });
 });
