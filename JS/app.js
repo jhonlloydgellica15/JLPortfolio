7d553b6319cb46d215eaeca97fd6a62907fad24c
JhonLloyd <jhonlloydgellica15@gmail.com>
2021-10-26 07:16:04 +0800

    Work experience added

--- JS/app.js
+++ JS/app.js
@@ -2,4 +2,8 @@ $(document).ready(() => {
   $(".fa-bars").click(() => {
     $(".main-navigation").slideToggle();
   });
+
+  $(".hello").click(() => {
+    $(".proj-description p").slideToggle();
+  });
 });
