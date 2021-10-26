ede5d1f8f67f7f793795f82ef7408bd4f45555ba
JhonLloyd <jhonlloydgellica15@gmail.com>
2021-10-27 07:38:28 +0800

    fixed margin overlap in footer section

--- JS/app.js
+++ JS/app.js
@@ -3,14 +3,27 @@ $(document).ready(() => {
     $(".main-navigation").slideToggle();
   });
 
-  $(".hello").click(() => {
-    $(".proj-description p").slideToggle();
-  });
-
   const toTop = $(".to-top");
   console.log(toTop);
   window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) toTop.addClass("active");
     else toTop.removeClass("active");
   });
+
+  let navItems = $("nav ul > li a");
+  let sections = $(".sections");
+
+  window.onscroll = () => {
+    let scrollHeight = window.pageYOffset;
+
+    for (let i = 0; i < sections.length; i++) {
+      if (sections[i].offsetTop / 1.1 <= scrollHeight) {
+        for (let j = 0; j < navItems.length; j++) {
+          navItems[j].classList.remove("active");
+        }
+
+        navItems[i].classList.add("active");
+      }
+    }
+  };
 });
